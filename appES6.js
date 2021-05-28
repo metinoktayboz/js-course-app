33cb430d3da0185a15073ee22beae0208d6155c8
Metin Oktay BOZ <metinoktayboz@gmail.com>
2021-05-28 16:34:12 +0300

    initial commit

---
+++ appES6.js
@@ -0,0 +1,153 @@
+//Course Class
+class Course{
+    constructor(title,instructor,image){
+        this.title=title;
+        this.instructor=instructor;
+        this.image=image;
+        this.courseID;
+        //this.courseID = Math.floor(Math.random()*100000);
+    }
+}
+
+//UI Class
+class UI{
+    addCourseToList(course){
+        const list = document.getElementById('course-list');
+
+        var html = `
+        <tr>
+            <td><img src="img/${course.image}"/></td>
+            <td>${course.title}</td>
+            <td>${course.instructor}</td>
+            <td><a href ="#" data-id="${course.courseID}" class="btn btn-danger btn-sm delete">Delete</a></td>
+        <tr>
+        `;
+
+        list.innerHTML += html;
+    }
+
+    clearControls(){
+        const title = document.getElementById('title').value="";
+        const instructor = document.getElementById('instructor').value="";
+        const image = document.getElementById('image').value="";
+    }
+
+    deleteCourse(element){
+        if(element.classList.contains('delete')){
+            element.parentElement.parentElement.remove();
+            return true;
+        }
+    }
+
+    showAlert(message,className){
+        var alert = `
+        <div class="alert alert-${className}">
+            ${message}
+        </div>
+        `;
+
+        const row = document.querySelector('.row');
+        // beforeBegin,  afterBegin,  beforeEnde , afterEnd
+        row.insertAdjacentHTML('beforeBegin',alert);
+
+        setTimeout(()=>{
+        document.querySelector('.alert').remove();
+        },1500);
+    }
+}
+
+class Storage{
+
+    static getCourses(){
+        let courses;
+
+        if(localStorage.getItem('courses')===null){
+            courses=[];
+        }else{
+            courses = JSON.parse(localStorage.getItem('courses'));
+        }
+
+        return courses;
+    }
+
+    static displayCourses(){
+        const courses = Storage.getCourses();
+
+        courses.forEach(course => {
+            const ui = new UI();
+            ui.addCourseToList(course);
+        });
+    }
+
+    static addCourse(course){
+        const courses = Storage.getCourses();
+        courses.push(course);
+        localStorage.setItem('courses',JSON.stringify(courses));
+    }
+
+    static deleteCourse(element){
+        if(element.classList.contains('delete')){
+            const id = element.getAttribute('data-id');
+            
+            const courses = Storage.getCourses();
+
+            courses.forEach((course,index) => {
+                if(course.courseID == id){
+                    courses.splice(index,1);
+                }
+            });
+
+            localStorage.setItem('courses', JSON.stringify(courses));
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded',Storage.displayCourses);
+
+let courseID = 0;
+document.getElementById('new-course').addEventListener('submit',function(e){
+    
+
+    const title = document.getElementById('title').value;
+    const instructor = document.getElementById('instructor').value;
+    const image = document.getElementById('image').value;
+
+    //create object
+    const course = new Course(title, instructor, image)
+    course.courseID=courseID++;
+    console.log(course);
+
+    //create UI
+    const ui = new UI();
+
+    if(title==='' || instructor==='' || image===''){
+        ui.showAlert('Please complete the form','warning');
+    }else{
+        //add course to list
+        ui.addCourseToList(course);
+
+        //save to local storage
+        Storage.addCourse(course);
+
+         //clear controls
+         ui.clearControls();
+
+         ui.showAlert('The course has been added.','success');
+    }
+
+    e.preventDefault();
+});
+
+
+document.getElementById('course-list').addEventListener('click',function(e){
+    
+    const ui = new UI();
+
+    //delete from list
+    if(ui.deleteCourse(e.target)==true){
+        //delete from LS
+    Storage.deleteCourse(e.target);
+    ui.showAlert('Course has been deleted.','danger');
+    }   
+
+});
